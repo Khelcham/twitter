c6c0a5faa51106ddf1e4b2e17bbebf8c4a0f0635
Khelcham Sadykova <sadikova197949@gmail.com>
2014-08-22 09:41:42 +0000

    chapter 10

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,10 +1,18 @@
 module SessionsHelper
-     
+    
   def sign_in(user)
     remember_token = User.new_remember_token
     cookies.permanent[:remember_token] = remember_token
     user.update_attribute(:remember_token, User.encrypt(remember_token))
     self.current_user = user
+  end
+  
+  
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
   end
   
   def signed_in?
@@ -19,6 +27,7 @@ module SessionsHelper
     @current_user = user
   end
 
+  
   def current_user
     remember_token = User.encrypt(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
@@ -27,7 +36,7 @@ module SessionsHelper
   def sign_out
     current_user.update_attribute(:remember_token,
                                   User.encrypt(User.new_remember_token))
-    cookies.delete(:rememberu_token)
+    cookies.delete(:remember_token)
     self.current_user = nil
   end
   
@@ -38,11 +47,5 @@ module SessionsHelper
 
   def store_location
     session[:return_to] = request.url if request.get?
-  end
-  def signed_in_user
-    unless signed_in?
-      store_location
-      redirect_to signin_url, notice: "Please sign in."
-    end
   end
 end
